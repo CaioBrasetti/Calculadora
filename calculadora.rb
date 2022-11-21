5683024637f34d1cab0ccc179cd361fe25037278
Caio Brasetti <caiobrasetti@hotmail.com>
2022-11-21 12:22:07 -0300

    Removendo comentario

--- calculadora.rb
+++ calculadora.rb
@@ -7,21 +7,6 @@ calculo = gets.chomp
 puts "Digite o segundo numero"
 numero2 = gets.chomp.to_f
 
-# if calculo == "+" or calculo == "soma" or calculo == "mais"
-#   numero1 = numero1 + numero2
-#   puts "Sua soma é #{numero1}"  
-# elsif calculo == "-" or calculo == "subtração" or calculo == "menos"
-#   numero1 = numero1 - numero2
-#   puts "Sua subitração é #{numero1}"
-# elsif calculo == "/" or calculo == "divisão" or calculo == "dividido"
-#   numero1 = numero1 / numero2
-#   puts "Sua divisão é #{numero1}"
-# elsif calculo == "*" or calculo == "multiplicação" or calculo == "vezes"
-#   numero1 = numero1 * numero2
-#   puts "Sua multiplicação é #{numero1}"
-# else
-#   puts "Calculo digitado está incorreto"
-# end
 
 case calculo
 when "+" , "soma" , "mais"
